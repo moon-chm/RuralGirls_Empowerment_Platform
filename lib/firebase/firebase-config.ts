77466f9a116929731f7f91e67be3559edf1855ef
moon-chm <your-github-email@example.com>
2026-01-09 00:41:37 +0530

    mistake revoked

--- lib/firebase/firebase-config.ts
+++ lib/firebase/firebase-config.ts
@@ -1,10 +1,10 @@
 // firebase-config.ts
 export const firebaseConfig = {
-  apiKey: "AIzaSyDc24OB9zAusNwbaSo2i9zqT_aiz-rMKSo",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
   authDomain: "rural-girls-platform.firebaseapp.com",
   projectId: "rural-girls-platform",
-  storageBucket: "rural-girls-platform.appspot.com", 
+  storageBucket: "rural-girls-platform.appspot.com",
   messagingSenderId: "514015547896",
   appId: "1:514015547896:web:a33ac8b546dd58cc84460d",
-  measurementId: "G-V8F96ZWXX7"
-}
+  measurementId: "G-V8F96ZWXX7",
+};
